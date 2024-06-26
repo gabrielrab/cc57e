4aa30e2fabd3a8b9d627b1a5f010008d926c60f9
tassi <84099851+tassid@users.noreply.github.com>
2024-06-26 14:19:00 -0300

    update SelectedMember.jsx

--- chat-webapp/src/components/Group/SelectedMember.jsx
+++ chat-webapp/src/components/Group/SelectedMember.jsx
@@ -4,7 +4,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 const SelectedMember = ({member,handleRemoveMember}) => {
   return (
     <div className='flex items-center bg-slate-300 rounded-full'>
-        <img className='w-7 h-7 rounded-full ' src={member.profile_picture ||"https://cdn.pixabay.com/photo/2022/10/06/13/17/monks-7502654__340.jpg"} alt="" />
+        <img className='w-7 h-7 rounded-full ' src={member.profile_picture ||"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"} alt="" />
         <p className='px-2'>{member.full_name}</p>
         <AiOutlineClose onClick={handleRemoveMember} className='pr-1 cursor-pointer'/>
     </div>
